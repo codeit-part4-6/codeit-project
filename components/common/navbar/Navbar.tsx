da613a054b9709440e912072859c505e0ed31504
whdjh <whdjh98@naver.com>
2025-01-17 02:26:52 +0900

    [feat]: 게스트일 경우 피그마 모든 요구사항 완료

--- components/common/navbar/Navbar.tsx
+++ components/common/navbar/Navbar.tsx
@@ -4,25 +4,28 @@ import navlogo from '@/public/img/img_navlogo.svg'
 
 export default function Navbar() {
   return (
-    <div className="sticky flex h-20 p-3">
-      <div className="flex w-full items-center justify-between">
-        <div>
-          <Link href="/">
-            <Image
-              src={navlogo}
-              alt="로고 아이콘"
-            />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center gap-6 text-[14px] font-[500] leading-[24px] text-[#1B1B1B]">
+    <>
+      <nav className="sticky flex h-[70px] px-[20px] py-[20px] md:px-[10px] md:py-[10px] md:gap-[21px]">
+        <div className="flex w-full items-center justify-between">
           <div>
-            <Link href='/signin'>로그인</Link>
+            <Link href="/">
+              <Image
+                src={navlogo}
+                alt="로고 아이콘"
+              />
+            </Link>
           </div>
-          <div>
-            <Link href='/signup'>회원가입</Link>
+          <div className="flex items-center justify-center gap-6 text-[14px] font-[500] leading-[24px] text-[#1B1B1B]">
+            <div>
+              <Link href='/signin'>로그인</Link>
+            </div>
+            <div>
+              <Link href='/signup'>회원가입</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </nav>
+      <hr className="border-b-[1px] border-#DDDDDD" />
+    </>
   );
 }
