bdaf6b9feb9c07fa97eb8e5e7519103127a9c682
lemon1335 <kimjinhui114@gmail.com>
2025-01-16 13:41:43 +0900

    [feat] 공통 컬러팔레트, 폰트 설정

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+  <>
+  <div>test page</div>
+    <div className='font-bold bg-gray-700 text-2xl'>test</div>
+    <div className='font-semibold text-secondary bg-primary text-xl'>test</div>
+    <div className='font-medium text-accent-green bg-orange-50 text-md'>test</div>
+    </>)
+};
+
+export default page;
