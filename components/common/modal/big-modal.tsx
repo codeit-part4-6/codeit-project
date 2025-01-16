350e244130ec74e7a0b18c60a398d7cbdab7a9e2
twtwkim <alfog23@gmail.com>
2025-01-16 16:33:09 +0900

    [style]: BigModal 반응형 적용

--- components/common/modal/big-modal.tsx
+++ components/common/modal/big-modal.tsx
@@ -8,9 +8,9 @@ interface ModalProps{
 export default function BigModal({message, onClose}: ModalProps){
     return (
         <OverlayContainer>
-          <div className="relative bg-white rounded-lg w-[540px] h-[250px] flex items-center justify-center">
-            <p className="text-2lg font-medium">{message}</p>
-            <div className="absolute" style={{ right: "28px", bottom: "28px" }}>
+          <div className="relative bg-white rounded-lg mx-6 w-full h-[220px] pc:w-[540px] pc:h-[250px] tablet:w-[540px] tablet:h-[250px] flex items-center justify-center">
+            <p className="text-lg pc:text-2lg tablet:text-2lg font-medium">{message}</p>
+            <div className="absolute bottom-7 right-7" >
             {/* 아래 button 태그는 공통 버튼으로 대체하세요 */}
             <button
               onClick={onClose}
