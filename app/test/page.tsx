070bd59b892ca5280ee108d4c80d2eb3d8b80384
twtwkim <alfog23@gmail.com>
2025-01-16 21:41:12 +0900

    [style]: pr 전 merge

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -22,13 +22,13 @@ const handleCloseModal = () => {
     <div className='font-semibold text-secondary bg-primary text-xl'>test</div>
     <div className='font-medium text-accent-green bg-orange-50 text-md'>test</div>
     <button onClick={handleOpenModal}>모달 열기</button>
-    {/* {isOpen && <ReviewModal message={"후기 작성"} onClose={handleCloseModal}/>} */}
+    {isOpen && <ReviewModal message={"후기 작성"} onClose={handleCloseModal}/>}
     {/* {isOpen && <Modal type="small" 
   message="예약을 취소하시겠습니까?" 
   onClose={handleCloseModal} />} */}
-    {isOpen && <Modal type="big" 
+    {/* {isOpen && <Modal type="big" 
   message="모달모달모달" 
-  onClose={handleCloseModal} />}
+  onClose={handleCloseModal} />} */}
     </>)
 };
 
