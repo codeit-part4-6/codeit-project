3638c033843c4cf866b660471b250c4f15dd7610
whdjh <whdjh98@naver.com>
2025-01-17 17:09:56 +0900

    [feat]: 로고 추가

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Navbar from '@/components/common/navbar/navbar';
+import signLogo from '@/public/img/img_signlogo.svg';
+
 export default function Page() {
   return (
-    <>
-      
-    </>
+    <main>
+      <Navbar />
+      <form>
+        <div>
+          <Link href="/">
+            <Image src={signLogo} alt="로고" />
+          </Link>
+        </div>
+      </form>
+    </main>
   )
 }
