da76650d4f6029284a1800f8c6b530e46f395179
whdjh <whdjh98@naver.com>
2025-01-17 23:52:41 +0900

    [style]: header main 분리

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -14,37 +14,39 @@ export default function Page() {
   const [inputPassword, setInputPassword] = useState('');
   
   return (
-    <main>
+    <>
       <Navbar />
-      <div className="flex flex-col items-center max-w-[640px] m-auto pt-[110px] gap-[24px] tablet:pt-[126px] tablet:gap-[40px] desktop:pt-[118px] destop:gap-[56px]">
-        <Link href="/">
-          <Image src={signLogo} alt="로고" />
-        </Link>
-        <form className="flex flex-col gap-[28px] w-full items-center justify-center">
-          <Input
-          label="이메일"
-          placeholder="이메일을 입력해 주세요"
-          labelClassName="block text-lg pb-2"
-          className=""
-          value={inputLogin}
-          onChange={e => setInputLogin(e.target.value)}
-          />
+      <main>
+        <div className="flex flex-col items-center max-w-[640px] m-auto pt-[110px] gap-[24px] tablet:pt-[126px] tablet:gap-[40px] desktop:pt-[118px] destop:gap-[56px]">
+          <Link href="/">
+            <Image src={signLogo} alt="로고" />
+          </Link>
+          <form className="flex flex-col gap-[28px] w-full items-center justify-center">
+            <Input
+            label="이메일"
+            placeholder="이메일을 입력해 주세요"
+            labelClassName="block text-lg pb-2"
+            className=""
+            value={inputLogin}
+            onChange={e => setInputLogin(e.target.value)}
+            />
 
-          <Input
-          label="비밀번호"
-          placeholder="비밀번호를 입력해 주세요"
-          labelClassName="block text-lg pb-2"
-          className=""
-          value={inputPassword}
-          onChange={e => setInputPassword(e.target.value)}
-          onBlur={e => {console.log(e);}}
-          />
+            <Input
+            label="비밀번호"
+            placeholder="비밀번호를 입력해 주세요"
+            labelClassName="block text-lg pb-2"
+            className=""
+            value={inputPassword}
+            onChange={e => setInputPassword(e.target.value)}
+            onBlur={e => {console.log(e);}}
+            />
 
-          <Button className="bg-primary text-white w-[350px] h-[54px] rounded-[6px] pt-[14px] pr-[136px] pb-[14px] pl-[136px] gap-[8px] sm:w-[300px] sm:px-4 tablet:w-[640px] tablet:h-[48px]">
-            검색하기
-          </Button>
-        </form>
-      </div>
-    </main>
+            <Button className="bg-primary text-white w-[350px] h-[54px] rounded-[6px] pt-[14px] pr-[136px] pb-[14px] pl-[136px] gap-[8px] sm:w-[300px] sm:px-4 tablet:w-[640px] tablet:h-[48px]">
+              검색하기
+            </Button>
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
