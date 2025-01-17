1863d58d1a27733c6f3af3592a1b75f0947fb267
whdjh <whdjh98@naver.com>
2025-01-18 00:27:55 +0900

    [feat]: 전체 레이아웃 완성

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,6 +6,8 @@ import Navbar from '@/components/common/navbar/navbar';
 import Input from '@/components/common/Input';
 import Button from '@/components/common/button';
 import signLogo from '@/public/img/img_signlogo.svg';
+import GoogleIcon from '@/public/icon/ic_google.svg';
+import KakaoIcon from '@/public/icon/ic_kakao.svg';
 import { useState } from 'react';
 
 export default function Page() {
@@ -27,7 +29,7 @@ export default function Page() {
                 label="이메일"
                 placeholder="이메일을 입력해 주세요"
                 labelClassName="block text-lg pb-2"
-                className=""
+                className="w-[350px] h-[58px] sm:w-[640px] sm:h-[58px]" 
                 value={inputLogin}
                 onChange={e => setInputLogin(e.target.value)}
                 />
@@ -36,7 +38,7 @@ export default function Page() {
                 label="비밀번호"
                 placeholder="비밀번호를 입력해 주세요"
                 labelClassName="block text-lg pb-2"
-                className=""
+                className="w-[350px] h-[58px] sm:w-[640px] sm:h-[58px]" 
                 value={inputPassword}
                 onChange={e => setInputPassword(e.target.value)}
                 onBlur={e => {console.log(e);}}
@@ -46,11 +48,24 @@ export default function Page() {
                   로그인 하기
                 </Button>
               </div>
-              <div className="text-[#4B4B4B] text-[16px]">
+              <span className="text-[#4B4B4B] text-[16px] font-[400] leading-[19.09px] text-center">
                 회원이 아니신가요?
                 <Link href="/signup" className="underline ml-[5px]">
                   회원가입하기
                 </Link>
+              </span>
+              <div className="flex flex-col gap-[24px] tablet:gap-[40px]">
+                <div className="flex items-center">
+                  <hr className="w-20 border border-gray-300" />
+                  <span className="text-[#79747E] text-[14px] leading-[24px] font-[400] text-center">
+                    SNS 계정으로 로그인하기
+                  </span>
+                  <hr className="w-20 border border-gray-300" />
+                </div>
+                <div className="flex gap-[16px]">
+                  <Image src={GoogleIcon} alt="google icon" />
+                  <Image src={KakaoIcon} alt="kakao icon" />
+                </div>
               </div>
             </div>
           </form>
