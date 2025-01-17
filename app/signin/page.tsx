b71ef42357f2dfd43dc6fea8a8232a3b2d0fc604
whdjh <whdjh98@naver.com>
2025-01-18 00:01:54 +0900

    [feat]: 요소 div 나누고 반응형 구현

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-
 import Image from 'next/image';
 import Link from 'next/link';
 import Navbar from '@/components/common/navbar/navbar';
@@ -21,29 +20,39 @@ export default function Page() {
           <Link href="/">
             <Image src={signLogo} alt="로고" />
           </Link>
-          <form className="flex flex-col gap-[28px] w-full items-center justify-center">
-            <Input
-            label="이메일"
-            placeholder="이메일을 입력해 주세요"
-            labelClassName="block text-lg pb-2"
-            className=""
-            value={inputLogin}
-            onChange={e => setInputLogin(e.target.value)}
-            />
+          <form className="flex flex-col gap-[40px] w-full items-center justify-center tablet:gap-[48px]">
+            <div className='flex flex-col gap-[26px] tablet:gap-[32px]'>
+              <div className='flex flex-col gap-[28px]'>
+                <Input
+                label="이메일"
+                placeholder="이메일을 입력해 주세요"
+                labelClassName="block text-lg pb-2"
+                className=""
+                value={inputLogin}
+                onChange={e => setInputLogin(e.target.value)}
+                />
 
-            <Input
-            label="비밀번호"
-            placeholder="비밀번호를 입력해 주세요"
-            labelClassName="block text-lg pb-2"
-            className=""
-            value={inputPassword}
-            onChange={e => setInputPassword(e.target.value)}
-            onBlur={e => {console.log(e);}}
-            />
+                <Input
+                label="비밀번호"
+                placeholder="비밀번호를 입력해 주세요"
+                labelClassName="block text-lg pb-2"
+                className=""
+                value={inputPassword}
+                onChange={e => setInputPassword(e.target.value)}
+                onBlur={e => {console.log(e);}}
+                />
 
-            <Button className="bg-primary text-white w-[350px] h-[54px] rounded-[6px] pt-[14px] pr-[136px] pb-[14px] pl-[136px] gap-[8px] sm:w-[300px] sm:px-4 tablet:w-[640px] tablet:h-[48px]">
-              검색하기
-            </Button>
+                <Button className="bg-primary text-white w-[350px] h-[54px] rounded-[6px] pt-[14px] pr-[136px] pb-[14px] pl-[136px] gap-[8px] sm:w-[300px] sm:px-4 tablet:w-[640px] tablet:h-[48px]">
+                  로그인 하기
+                </Button>
+              </div>
+              <div className="text-[#4B4B4B] text-[16px]">
+                회원이 아니신가요?
+                <Link href="/signup" className="underline ml-[5px]">
+                  회원가입하기
+                </Link>
+              </div>
+            </div>
           </form>
         </div>
       </main>
