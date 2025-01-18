7f60e2a791c40bfd2242ebbfba908539810923bf
whdjh <whdjh98@naver.com>
2025-01-19 01:30:01 +0900

    [feat]: 내정보, 저장하기 버튼 구현

--- app/components/mypage.tsx
+++ app/components/mypage.tsx
@@ -1,6 +1,24 @@
+'use client';
+
+import Input from '@/components/common/Input';
+import Button from '@/components/common/button';
+// import { useForm, Controller } from 'react-hook-form';
+
 export default function mypage() {
   return (
-    <>
-    </>
+    <div>
+      <div className="flex justify-between">
+        <h1 className="text-[32px] font-[700] leading-[42px] tablet:text-[38.19px] tablet:leading-[38.19px]">
+          내 정보
+        </h1>
+        <Button
+          className="bg-primary text-white w-[120px] h-[48px] rounded-[4px] pt-[8px] pr-[16px] pb-[8px] pl-[16px] gap-[4px]"
+          type="submit"
+        >
+          저장하기
+        </Button>
+      </div>
+      
+    </div>
   );
 }
