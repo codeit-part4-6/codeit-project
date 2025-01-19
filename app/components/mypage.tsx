cf97947caa10ff1517e5f80415daba08a1457fc7
whdjh <whdjh98@naver.com>
2025-01-20 02:22:00 +0900

    [feat]: 반응형 작업 완료

--- app/components/mypage.tsx
+++ app/components/mypage.tsx
@@ -34,7 +34,7 @@ export default function mypage() {
   const passwordValue = watch('password');
 
   return (
-    <div className="w-[792px] h-[472px] gap-[32px]">
+    <div className="w-[21.875rem] h-[472px] gap-[32px] tablet:w-[40rem]">
       <div className="flex justify-between">
         <p className="text-[32px] font-[700] leading-[42px] tablet:text-[38.19px] tablet:leading-[38.19px]">내 정보</p>
         <Button className="h-[48px] w-[120px] gap-[4px] rounded-[4px] bg-primary pb-[8px] pl-[16px] pr-[16px] pt-[8px] text-white" type="submit">
@@ -77,7 +77,7 @@ export default function mypage() {
               label="이메일"
               placeholder="이메일을 입력해 주세요"
               labelClassName="block text-lg pb-2"
-              className="h-[3.625rem] w-[31.875rem] tablet:h-[3.625rem] tablet:w-[40rem]"
+              className="h-[3.625rem] w-[21.875rem] tablet:h-[3.625rem] tablet:w-[40rem]"
               value={field.value}
               onChange={field.onChange}
               error={errors.email?.message}
