2edaafb5e285b6ab993b7df267f1844c48ead464
twtwkim <alfog23@gmail.com>
2025-01-19 12:29:15 +0900

    [style]: 모달 관련련 일부 스타일 조정

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,46 +1,48 @@
 'use client';
+import OverlayContainer from '@/components/common/modal/overlay-container';
 import SideNavi from '@/components/common/side-navigation/side-navi';
+import Test from '@/components/common/side-navigation/test';
 import React, {useState, useEffect} from 'react';
 
 export default function Page() {
   const [selectedMenu, setSelectedMenu] = useState('myinfo');
-  const [isMobile, setIsMobile] = useState<boolean>(getPageSize(window.innerWidth));
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   function getPageSize(width: number): boolean {
     return width < 745;
   }
 
   useEffect(() => {
-    const handleResize = () => {
-      const mobile = getPageSize(window.innerWidth);
-      setIsMobile(mobile);
-
-      if (!mobile) {
-        setSelectedMenu('myinfo');
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    const initialIsMobil = getPageSize(window.innerWidth);
+    setIsMobile(initialIsMobil); // 브라우저에서만 실행
   }, []);
 
   useEffect(() => {
-    if (isMobile) {
-      setSelectedMenu('');
+    const handleResize = () => {
+      const mobile = getPageSize(window.innerWidth);
+      setIsMobile(mobile);
+      setIsOpen(false);
+      if (!mobile && selectedMenu === '') {
+        setSelectedMenu('myinfo');
+      }
+    };
+    if (isMobile !== null) {
+      // isMobile이 null이 아니면 resize 이벤트 처리
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
     }
-  }, [isMobile]);
+  }, [selectedMenu, isMobile]);
 
   const renderContent = () => {
     switch (selectedMenu) {
       case 'myinfo':
         return (
           <div>
-            {' '}
-            {/*여기 컴포넌트 갈아끼워야해요*/}내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보
-            컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내 정보 컴포넌트내
-            정보 컴포넌트내 정보 컴포넌트
+            {/*여기 컴포넌트 갈아끼워야해요*/}
+            <Test />
           </div>
         );
       case 'reserveList':
@@ -66,12 +68,21 @@ export default function Page() {
     }
   };
 
+  if (isMobile === null) {
+    return null; // 클라이언트에서 isMobile 값을 정할 때까지 아무것도 렌더링하지 않음
+  }
+
   return (
     <div className="px-4 pt-6 tablet:p-6 pc:mt-[4.5rem] pc:w-full pc:max-w-[75rem] pc:p-0">
       {isMobile ? (
         // **모바일 환경**
         <div>
-          <SideNavi selectedMenu={selectedMenu} onSelectMenu={setSelectedMenu} isMobile={isMobile} />
+          <SideNavi selectedMenu={selectedMenu} onSelectMenu={setSelectedMenu} isMobile={isMobile} onOpenModal={() => setIsOpen(true)} />
+          {isOpen && (
+            <OverlayContainer>
+              <div className="h-full w-full bg-white">{renderContent()}</div>
+            </OverlayContainer>
+          )}
         </div>
       ) : (
         // **PC/태블릿 환경**
