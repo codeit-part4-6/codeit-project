03f61b10a5041c3210cb288260cd40ed62322c47
whdjh <whdjh98@naver.com>
2025-01-21 12:04:11 +0900

    [feat]: api 함수 구성

--- service/api/users/postSignup.api.ts
+++ service/api/users/postSignup.api.ts
@@ -1,7 +1,6 @@
 import INSTANCE_URL from '../instance';
-import {SignupBody, SignupResponse} from '../../../types/postSignup.types';
+import {SignupBody, SignupResponse} from '@/types/postSignup.types';
 
-// 회원가입
 export async function postSignup(body: SignupBody): Promise<SignupResponse> {
   const response = await INSTANCE_URL.post('/users', body);
   return response.data;
