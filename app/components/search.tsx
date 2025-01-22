0781d98a8e85d87bfef32bd24fcec56fee95367d
lemon1335 <kimjinhui114@gmail.com>
2025-01-22 19:14:53 +0900

    [feat] 반응형 작업 1차

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -4,17 +4,23 @@ import SearchIcon from '@/public/icon/ic_search.svg';
 
 const Search = () => {
   return (
-    <div className="flex flex-col items-center justify-center pt-14pxr">
-      <div className="absolute flex h-auto w-auto flex-col gap-15pxr rounded-2xl bg-white px-24pxr py-16pxr shadow-lg">
-        <h2 className="text-lg font-bold text-nomad-black">무엇을 체험하고 싶으신가요?</h2>
+    <div className="flex flex-col items-center justify-center">
+      <div className="absolute flex h-auto w-11/12 flex-col gap-15pxr rounded-2xl bg-white px-24pxr py-16pxr shadow-lg tablet:gap-8 tablet:px-6 tablet:py-8">
+        <h2 className="text-lg font-bold text-nomad-black tablet:text-xl">무엇을 체험하고 싶으신가요?</h2>
         <div className="flex justify-between gap-10pxr">
-          <div className="relative flex items-center rounded border border-solid border-gray-700">
+          <div className="relative flex w-full items-center rounded border border-solid border-gray-700 pl-2.5">
             <Image src={SearchIcon} alt="검색" width={24} height={24} className="absolute" />
             <label htmlFor="search">
-              <input type="search" placeholder="내가 원하는 체험은" className="pl-27pxr text-[0.875rem]/[1.625rem] font-regular text-gray-500" />
+              <input
+                type="search"
+                placeholder="내가 원하는 체험은"
+                className="pl-30pxr text-[0.875rem]/[1.625rem] font-regular text-gray-500 tablet:text-lg"
+              />
             </label>
           </div>
-          <Button className="h-56pxr w-auto rounded bg-nomad-black px-20pxr py-8pxr text-lg font-bold text-white">검색하기</Button>
+          <Button className="inline-flex h-56pxr w-auto items-center whitespace-nowrap rounded bg-nomad-black px-20pxr py-8pxr text-lg font-bold text-white tablet:px-10 tablet:py-2">
+            검색하기
+          </Button>
         </div>
       </div>
     </div>
